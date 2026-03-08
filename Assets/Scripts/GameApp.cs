8231ab612c5382c6c96958f7d920b6fade18d24a
qiqizizzz <qiqizizzz@qq.com>
2026-03-08 20:03:46 +0800

    初步创建场景 更换鼠标

---
+++ Assets/Scripts/GameApp.cs
@@ -0,0 +1,33 @@
+﻿/*
+* ┌──────────────────────────────────┐
+* │  描    述: 统一定义游戏内的管理器                      
+* │  类    名: GameApp.cs       
+* │  创    建: By qiqizizzz
+* └──────────────────────────────────┘
+*/
+
+using Common;
+using MVC;
+
+public class GameApp : Singleton<GameApp>
+{
+    public static ControllerManager ControllerManager;
+    public static ViewManager ViewManager;
+        
+    public override void Init()
+    {
+        ControllerManager = new ControllerManager();
+        ViewManager = new ViewManager();
+    }
+
+    public override void Update(float dt)
+    {
+            
+    }
+
+    public override void Destroy()
+    {
+            
+    }
+}
+    
---
+++ Assets/Scripts/GameScene.cs
@@ -0,0 +1,58 @@
+﻿/*
+* ┌───────────────────────────────────────────────────────────────────┐
+* │  描    述: 继承mono的脚本，需要挂载游戏物体，跳转场景后当前脚本物体不删除                      
+* │  类    名: GameScene.cs       
+* │  创    建: By qiqizizzz
+* └───────────────────────────────────────────────────────────────────┘
+*/
+
+using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class GameScene : MonoBehaviour
+    {
+        public Texture2D commonCursor;// 鼠标指针图片
+        
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+            GameApp.Instance.Init();
+        }
+
+        private void Start()
+        {
+            Cursor.SetCursor(commonCursor, Vector2.zero, CursorMode.Auto);
+            
+            RegisterConfigs();
+            RegisterModule();
+            InitModules();
+        }
+
+        private void Update()
+        {
+            float dt = Time.deltaTime;
+            GameApp.Instance.Update(dt);
+        }
+
+        // 注册控制器
+        private void RegisterModule()
+        {
+            
+        }
+        
+        // 初始化所有控制器
+        private void InitModules()
+        {
+            GameApp.ControllerManager.Init();
+        }
+        
+        // 注册配置表
+        private void RegisterConfigs()
+        {
+            
+        }
+        
+    }
+}
