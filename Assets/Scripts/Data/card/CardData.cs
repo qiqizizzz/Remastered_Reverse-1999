86ca05b6fd25a2b8243f699de5a3dae8c310cfde
qiqizizzz <qiqizizzz@qq.com>
2026-03-29 12:17:09 +0800

    定义角色卡牌与卡牌数据

---
+++ Assets/Scripts/Data/card/CardData.cs
@@ -0,0 +1,57 @@
+﻿/*
+* ┌──────────────────────────────────┐
+* │  描    述: 卡牌数据                      
+* │  类    名: CardData.cs       
+* │  创    建: By qiqizizzz
+* └──────────────────────────────────┘
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace Data.card
+{
+    /// <summary>
+    /// 卡牌类型
+    /// </summary>
+    public enum CardType
+    {
+        Attack,//攻击
+        Buff,//给己方的增益
+        Debuff,//给敌人的减益
+        Channel,//吟诵(特殊)
+        Health,//治疗
+        Ultimate//大招
+    }
+
+    /// <summary>
+    /// 卡牌效果
+    /// </summary>
+    [Serializable]
+    public class CardEffect
+    {
+        public float value;//倍率等
+        public int round;//持续回合数,如果是0则表示即时生效
+        public string target;//目标,可以是单体/全体/随机n个等
+    }
+    
+    [Serializable]
+    public class CardDataArray
+    {
+        public List<CardData> cards;
+    }
+    
+    [Serializable]
+    public class CardData
+    {
+        public CardType type;
+        public int id;
+        public string name;
+        public string description;
+        public int ownerId;//拥有者id
+
+        public CardEffect[] effects;
+
+        //TODO: 对玩家的属性进行增加以及特殊效果
+    }
+}
---
+++ Assets/Scripts/Data/card/CharacterData.cs
@@ -0,0 +1,86 @@
+﻿/*
+* ┌──────────────────────────────────┐
+* │  描    述: 角色数据                      
+* │  类    名: CharacterData.cs       
+* │  创    建: By qiqizizzz
+* └──────────────────────────────────┘
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace Data.card
+{
+    /// <summary>
+    /// 角色种类 - hero/monster
+    /// </summary>
+    public enum CharacterType
+    {
+        Hero,
+        Monster
+    }
+
+    /// <summary>
+    /// 灵感种类
+    /// 克制关系为 兽 -> 木 -> 星 -> 岩 -> 兽
+    /// </summary>
+    public enum InspirationType
+    {
+        /// <summary>
+        /// 兽
+        /// </summary>
+        Beast,
+        /// <summary>
+        /// 木
+        /// </summary>
+        Wood,
+        /// <summary>
+        /// 星
+        /// </summary>
+        Star,
+        /// <summary>
+        /// 岩
+        /// </summary>
+        Rock
+    }
+
+    /// <summary>
+    /// 玩家属性
+    /// </summary>
+    [Serializable]
+    public class Property
+    {
+        public float attack;//攻击
+        public float hp;//生命
+        public float defense;//防御
+        public float critRate;//暴击率
+        public float critDamage;//暴击伤害
+    }
+
+    [Serializable]
+    public class CharacterDataArray
+    {
+        public List<CharacterData> characters;
+    }
+    
+    [Serializable]
+    public class CharacterData
+    {
+        public int id;//角色id
+        public string name;//角色名字
+        public CharacterType characterType;//角色种类
+        public InspirationType inspirationType;//角色的灵感种类
+        
+        public Property property;//角色属性
+
+        public List<int> cards;//角色拥有的卡牌(一张大招牌和两张普通牌)
+
+        public CharacterData()
+        {
+            
+        }
+        
+        
+        //TODO:实例化
+    }
+}
