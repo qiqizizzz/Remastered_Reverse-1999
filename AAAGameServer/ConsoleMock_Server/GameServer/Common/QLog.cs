2816be1d5f9e3bc7001e4ff48a7604a5c0e2d8b2
qiqizizzz <qiqizizzz@qq.com>
2026-05-14 14:18:30 +0800

    服务端去除 [Conditional("DEBUG")]

--- AAAGameServer/ConsoleMock_Server/GameServer/Common/QLog.cs
+++ AAAGameServer/ConsoleMock_Server/GameServer/Common/QLog.cs
@@ -1,22 +1,19 @@
 /*
 * ┌──────────────────────────────────┐
-* │  描    述: 服务端轻量日志封装，Release 构建自动抹除 Info/Warning
+* │  描    述: 服务端轻量日志封装
 * │  类    名: QLog.cs
 * │  创    建: By qiqizizzz
 * └──────────────────────────────────┘
 */
 
 using System;
-using System.Diagnostics;
 
 namespace GameServer.Common
 {
     public static class QLog
     {
-        [Conditional("DEBUG")]
-        public static void Info(object msg) => Console.WriteLine($"[INFO] {msg}");
+        public static void Info(object msg) => Console.WriteLine(msg);
 
-        [Conditional("DEBUG")]
         public static void Warning(object msg) => Console.WriteLine($"[WARN] {msg}");
 
         public static void Error(object msg) => Console.Error.WriteLine($"[ERROR] {msg}");
